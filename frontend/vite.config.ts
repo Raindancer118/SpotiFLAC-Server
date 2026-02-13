1c3633fcce632e3336247b126fccdbcfcc110f35
Raindancer118 <tom560stieh@gmail.com>
2026-02-13 17:32:55 +0100

    refactor: get app version from package.json instead of wails.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const wailsJsonPath = path.resolve(__dirname, "../wails.json");
-const wailsJson = JSON.parse(fs.readFileSync(wailsJsonPath, "utf-8"));
-const appVersion = wailsJson.info.productVersion;
+const packageJsonPath = path.resolve(__dirname, "./package.json");
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+const appVersion = packageJson.version;
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
